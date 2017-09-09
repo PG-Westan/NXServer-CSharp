ec0439e092435101514d5ab426b56c6a91576330
Westan <magi_weijian@foxmail.com>
2017-09-09 18:16:55 +0800

    Init Project
    add lib superSocket  json protobuf

---
+++ NXServer-CSharp/ServerMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NXServer_CSharp
+{
+    public class ServerMain
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
